43a28c8707d60bccce6194cd07868dc1de500e74
Alex34972 <alex_hifi@ukr.net>
2021-07-08 21:35:06 +0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Profile from "./components/profile";
+import user from './user.json';
+export default function App (){
+  return ( <Profile
+  avatar={user.avatar}
+  name={user.name}
+  tag={user.tag}
+  location={user.location}
+  followers={user.stats.followers}
+  views={user.stats.views}
+  likes={user.stats.likes}
+  />);
+}
