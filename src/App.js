dd1543aa4594771dff423c1243ef4671c37bef9f
Alex34972 <alex_hifi@ukr.net>
2021-07-11 18:43:18 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import Profile from "./components/profile";
+import Profile from './components/profile';
+import Statistics from './components/statistics';
+import FriendList from './components/friendList';
+import TransactionHistory from './components/transactionHistory';
 import user from './user.json';
-export default function App (){
-  return ( <Profile
-  avatar={user.avatar}
-  name={user.name}
-  tag={user.tag}
-  location={user.location}
-  followers={user.stats.followers}
-  views={user.stats.views}
-  likes={user.stats.likes}
-  />);
+import statisticalData from './statistic-data.json';
+import friends from './friends.json';
+import transactions from './transactions.json'
+export default function App() {
+  return (
+    <div>
+      <Profile
+        avatar={user.avatar}
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+      <Statistics title="Upload stats" stats={statisticalData} />
+      <Statistics stats={statisticalData} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </div>
+  );
 }
