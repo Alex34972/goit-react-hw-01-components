2a1f4eb4902a55c875d3f517e6838676069ae6fb
Alex34972 <alex_hifi@ukr.net>
2021-07-11 18:43:24 +0300

    Create friendList.js

---
+++ src/components/friendList.js
@@ -0,0 +1,17 @@
+import propTypes from 'prop-types';
+export default function FriendList({ friends }) {
+ return( <ul class="friend-list">
+    {friends.map(friend => (
+      <li class="item">
+        <span class="status">{friend.isOnline}</span>
+        <img class="avatar" src={friend.avatar} alt="" width="48" />
+        <p class="name">{friend.name}</p>
+      </li>
+    ))}
+  </ul>);
+}
+FriendList.propTypes = {
+    isOnline:propTypes.string,
+    avatar:propTypes.string,
+    name:propTypes.string
+}
