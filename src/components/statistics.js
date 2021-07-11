8c23e05bc2b870e9b5bb76f27b6f689cdf536a10
Alex34972 <alex_hifi@ukr.net>
2021-07-11 18:43:28 +0300

    Create statistics.js

---
+++ src/components/statistics.js
@@ -0,0 +1,21 @@
+import propTypes from 'prop-types';
+export default function Statistics({title, stats} ) {
+  return (
+    <section class="statistics">
+      {title && <h2 class="title">{title}</h2>}
+
+      <ul class="stat-list">
+        {stats.map(stat => (
+          <li class="item">
+            <span class="label">{stat.label}</span>
+            <span class="percentage">{stat.percentage}</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+Statistics.propTypes = {
+  label: propTypes.string.isRequired,
+  percentage: propTypes.number.isRequired
+};
