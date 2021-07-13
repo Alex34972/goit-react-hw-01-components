d85785922dd8a1d34ba10c90f9d4263be4ba23fb
Alex34972 <alex_hifi@ukr.net>
2021-07-13 17:11:54 +0300

    Create statisticsProfile.js

---
+++ src/components/profile/statisticsProfile.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import s from './profile.module.css';
+
+const StatisticsProfile = ({ followers, views, likes }) => {
+  return (
+    <ul className={s.stats}>
+      <li>
+        <span className={s.label}>Followers</span>
+        <span className={s.quantity}>{followers}</span>
+      </li>
+      <li>
+        <span className={s.label}>Views</span>
+        <span className={s.quantity}>{views}</span>
+      </li>
+      <li>
+        <span className={s.label}>Likes</span>
+        <span className={s.quantity}>{likes}</span>
+      </li>
+    </ul>
+  );
+};
+
+export default StatisticsProfile;
