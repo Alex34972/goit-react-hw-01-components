1afe7301ff5cdf242071dde5a269a7583ed49be6
Alex34972 <alex_hifi@ukr.net>
2021-07-13 17:11:43 +0300

    Update statistics.js

--- src/components/statistics/statistics.js
+++ src/components/statistics/statistics.js
@@ -6,18 +6,21 @@ export default function Statistics({ title, stats }) {
       {title && <h2 className={s.title}>{title}</h2>}
 
       <ul className={s.statList}>
-        {stats.map(stat => (
-          <li className={s.item} key={stat.id}>
-            <span className={s.label}>{stat.label}</span>
-            <span className={s.percentage}>{stat.percentage}%</span>
+        {stats.map(({ id, label, percentage }) => (
+          <li className={s.item} key={id}>
+            <span className={s.label}>{label}</span>
+            <span className={s.percentage}>{percentage}%</span>
           </li>
         ))}
       </ul>
     </section>
   );
 }
-
+Statistics.defaultProps = {
+  title: ' ',
+};
 Statistics.propTypes = {
+  title: PropTypes.string,
   stats: PropTypes.arrayOf(
     PropTypes.shape({
       percentage: PropTypes.number.isRequired,
